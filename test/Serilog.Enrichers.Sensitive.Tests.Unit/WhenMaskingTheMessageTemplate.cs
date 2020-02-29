0a37f78b19755214c135af9e0bae39441d7acdfe
Sander van Vliet <sander@codenizer.nl>
2020-02-29 13:28:42 +0100

    Add some tests

---
+++ test/Serilog.Enrichers.Sensitive.Tests.Unit/WhenMaskingTheMessageTemplate.cs
@@ -0,0 +1,65 @@
+﻿using Serilog.Sinks.InMemory;
+using Serilog.Sinks.InMemory.Assertions;
+using Xunit;
+
+namespace Serilog.Enrichers.Sensitive.Tests.Unit
+{
+    public class WhenMaskingTheMessageTemplate
+    {
+        [Fact]
+        public void GivenNotInSensitiveArea_EmailAddressIsNotMasked()
+        {
+            _logger.Information("test@email.com");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("test@email.com");
+        }
+
+        [Fact]
+        public void GivenNotInSensitiveArea_IbanIsNotMasked()
+        {
+            _logger.Information("NL02ABNA0123456789");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("NL02ABNA0123456789");
+        }
+
+        [Fact]
+        public void GivenInSensitiveArea_EmailAddressIsMasked()
+        {
+            using (_logger.EnterSensitiveArea())
+            {
+                _logger.Information("test@email.com");
+            }
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("***MASKED***");
+        }
+
+        [Fact]
+        public void GivenInSensitiveArea_IbanIsMasked()
+        {
+            using (_logger.EnterSensitiveArea())
+            {
+                _logger.Information("NL02ABNA0123456789");
+            }
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("***MASKED***");
+        }
+
+        private readonly ILogger _logger;
+
+        public WhenMaskingTheMessageTemplate()
+        {
+            _logger = new LoggerConfiguration()
+                .Enrich.WithSensitiveDataMasking()
+                .WriteTo.InMemory()
+                .CreateLogger();
+        }
+    }
+}
