99da31c2a1cdb16c80aac7413e6d8ee01057146a
Sander van Vliet <sander@codenizer.nl>
2022-09-25 12:00:12 +0200

    WIP

---
+++ test/Serilog.Enrichers.Sensitive.Tests.Unit/WhenUsingSinkInsteadOfEnricher.cs
@@ -0,0 +1,55 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.InMemory;
+using Serilog.Sinks.InMemory.Assertions;
+using Xunit;
+
+namespace Serilog.Enrichers.Sensitive.Tests.Unit
+{
+    public class WhenUsingSinkInsteadOfEnricher
+    {
+        [Fact]
+        public void Dwim()
+        {
+            var inMemorySink = new InMemorySink();
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Sink(inMemorySink)
+                .MaskSensitiveData()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            logger.Information("Test message {Email}", "joe.blogs@example.com");
+            
+            inMemorySink
+                .Should()
+                .HaveMessage("Test message {Email}")
+                .Appearing().Once()
+                .WithProperty("Email")
+                .WithValue("***MASKED***");
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static LoggerConfiguration MaskSensitiveData(this LoggerConfiguration configuration)
+        {
+            var maskingSink = new MaskingSink(configuration);
+
+            return configuration
+                .WriteTo.Sink(maskingSink);
+        }
+    }
+
+    public class MaskingSink : ILogEventSink
+    {
+        public MaskingSink(LoggerConfiguration configuration)
+        {
+            
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+        }
+    }
+}
